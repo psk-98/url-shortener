1ceebc6c1ac9180c8ca6ef3d00dd82dfe1b3ffe9
Paul <paul@vetro.co.za>
2026-04-20 18:43:41 +0200

    add clearer relationship between visit and redirects, and create top redirects routes

--- backend/app/schema/redirects.py
+++ backend/app/schema/redirects.py
@@ -1,10 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+
+from app.schema.visits import VisitResponse
+
+
+class RedirectTopResponse(BaseModel):
+    id: int
+    alias: str
+    url: str
+    visit_count: int
 
 
 class RedirectResponse(BaseModel):
     id: int
     alias: str
     url: str
+    visits: list[VisitResponse] = []
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class CreateRedirectRequest(BaseModel):
--- backend/app/schema/visits.py
+++ backend/app/schema/visits.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class VisitResponse(BaseModel):
+    id: int
     created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class CreateVisitRequest(BaseModel):
