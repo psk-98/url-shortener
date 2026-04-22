d8e9c11b08687c4432722c5853a72f8ec38c0f59
Paul <paul@vetro.co.za>
2026-04-22 20:42:33 +0200

    basic landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,65 +1,45 @@
-import Image from "next/image";
+import RedirectsTable from "@/components/home/redirectsTable"
+import URLForm from "@/components/home/urlForm"
 
-export default function Home() {
+export default async function Home() {
+  const redirects = await getRedirects()
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      {console.log(redirects)}
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+        <h1 className="mt-16 text-4xl text-center">
+          {"Shorten Your Loooong Links:)"}
+        </h1>
+        <p className="my-4 text-center">
+          Linkly is an efficient and easy-to-use URL shortening service that
+          streamlines your online experience
+        </p>
+
+        <URLForm />
+
+        {/* <div className="space-y-2">
+				<div className="flex items-center space-x-2">
+					<Switch
+						id="airplane-mode"
+						checked={isAutoCopyUrl}
+						onCheckedChange={setIsAutoCopyUrl}
+					/>
+					<Label htmlFor="airplane-mode">Auto Copy To Clipboard</Label>
+				</div>
+			</div> */}
+
+        <RedirectsTable redirects={redirects} />
       </main>
     </div>
-  );
+  )
+}
+
+const getRedirects = async () => {
+  const res = await fetch("http://127.0.0.1:8000/api/v1/redirects/top")
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch top redirects")
+  }
+
+  return res.json()
 }
---
+++ frontend/src/components/home/urlForm.tsx
@@ -0,0 +1,98 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { IconCheck, IconCopy } from "@tabler/icons-react"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { useCopyToClipboard } from "@/hooks/useCopyToClipboard"
+import { type URLFormData, urlFormSchema } from "@/schemas/urlForm.schema"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from "../ui/input-group"
+
+export default function URLForm() {
+  const { copyToClipboard, isCopied } = useCopyToClipboard()
+  const [shortenedURL, setShortenedURL] = useState<string | null>(null)
+
+  const baseApiUrl = process.env.NEXT_PUBLIC_API_URL
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<URLFormData>({
+    resolver: zodResolver(urlFormSchema),
+    defaultValues: { url: "" },
+  })
+
+  const onSubmit = async (values: URLFormData) => {
+    const res = await fetch(`${baseApiUrl}/redirects/guest_redirect`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(values),
+    })
+
+    if (!res.ok) {
+      console.log(res)
+      throw new Error(`HTTP error: ${res.status}`)
+    }
+
+    const data = await res.json()
+    setShortenedURL(data.alias)
+    console.log(data)
+  }
+
+  return (
+    <form className="mb-6 min-w-10/12" onSubmit={handleSubmit(onSubmit)}>
+      {console.log(baseApiUrl)}
+      {shortenedURL ? (
+        <InputGroup>
+          <InputGroupInput placeholder={`${baseUrl}${shortenedURL}`} readOnly />
+          <InputGroupAddon align="inline-end">
+            <InputGroupButton
+              type="button"
+              aria-label="Copy"
+              title="Copy"
+              size="icon-xs"
+              onClick={() => {
+                copyToClipboard(`${baseUrl}${shortenedURL}`)
+              }}
+            >
+              {isCopied ? <IconCheck /> : <IconCopy />}
+            </InputGroupButton>
+          </InputGroupAddon>
+        </InputGroup>
+      ) : (
+        <div className="space-y-1">
+          <InputGroup>
+            <InputGroupInput
+              {...register("url")}
+              placeholder="Enter a url.."
+              aria-invalid={!!errors.url}
+            />
+            <InputGroupAddon align="inline-end">
+              <InputGroupButton
+                type="submit"
+                variant="secondary"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? "Shortening..." : "Shorten"}
+              </InputGroupButton>
+            </InputGroupAddon>
+          </InputGroup>
+
+          {errors.url && (
+            <p className="text-sm text-destructive">{errors.url.message}</p>
+          )}
+        </div>
+      )}
+    </form>
+  )
+}
