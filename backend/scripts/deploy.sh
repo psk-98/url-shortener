2341a7f90dfa364a3299c4ca59185fa8e8918e4b
Paul <paul@vetro.co.za>
2026-07-22 14:05:48 +0200

    Update deploy.sh

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -13,5 +13,5 @@ git reset --hard "origin/trunk"
 echo "Going into backend dir"
 cd /home/lullaby/sites/url-shortener/backend
 
-cp /home/lullaby/sites/url-shortener/backend/.docker/docker-composer.prod.yml /home/lullaby/sites/url-shortener/backend/docker-compose.yml
+cp .docker/docker-compose.prod.yml docker-compose.yml
 sudo docker compose up -d --build
